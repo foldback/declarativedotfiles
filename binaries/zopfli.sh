38a8399dfc7b0b9dbd78ab23113839f6d39c1439
Jorrit Visser <jorritjanvisser@gmail.com>
2017-06-19 20:03:42 +0200

    Add `zopfli` installer

---
+++ binaries/zopfli.sh
@@ -0,0 +1,18 @@
+# macOS
+# zopfli.sh
+# © Jorrit Visser <git.io/jorvi>
+
+
+main() {
+  # Load the magic library
+  source "${HOME}/dotfiles/scripts/ddfunlib"
+
+  # Declare name, package name, etc.
+  Name="Zopfli"
+  Packagename="zopfli"
+
+  # Abracadabra
+  brewBinary
+}
+
+main
